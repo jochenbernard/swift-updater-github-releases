925cab37d3f3ac76410f2e5585c0a4d7e917b770
Jochen Bernard <jochenbernard@gmail.com>
2025-08-20 20:35:30 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,26 +1,41 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftUpdaterGitHubReleases",
+    platforms: [.macOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUpdaterGitHubReleases",
             targets: ["SwiftUpdaterGitHubReleases"]
-        ),
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/SimplyDanny/SwiftLintPlugins",
+            from: "0.1.0"
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUpdaterGitHubReleases"
+            name: "SwiftUpdaterGitHubReleases",
+            plugins: [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLintPlugins"
+                )
+            ]
         ),
         .testTarget(
             name: "SwiftUpdaterGitHubReleasesTests",
-            dependencies: ["SwiftUpdaterGitHubReleases"]
-        ),
+            dependencies: ["SwiftUpdaterGitHubReleases"],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintBuildToolPlugin",
+                    package: "SwiftLintPlugins"
+                )
+            ]
+        )
     ]
 )
